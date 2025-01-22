aef421fb0679e34e53b82402215933521c3b2d0d
jbite <jbie9057@gmail.com>
2025-01-22 17:01:34 +0800

    admin

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Form
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "email", "date", "occupation")
+    list_filter = ("first_name", "last_name", "email", "date", "occupation")
+    ordering = ("first_name", )
+    readonly_fields = ("occupation",)
+    
+    
+admin.site.register(Form, FormAdmin)
